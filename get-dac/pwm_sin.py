c469f5d604932f1a0fc12fefa82ae59c66590cf0
LeonidYutlin <leonidyutlin@phystech.edu>
2026-02-18 19:39:43 +0300

    sine pwm

---
+++ get-dac/pwm_sin.py
@@ -0,0 +1,18 @@
+import pwm_dac as pwm
+import signal_gen as sg
+import time
+
+ampl = 3.0
+freq = 50
+samp_freq = 1000
+
+try:
+    dac = pwm.PWM_DAC(12, 5000, 3.290, True)
+    while True:
+        try:
+            dac.set_vol(sg.get_sin_wave_ampl(freq, time.time()) * ampl)
+            sg.wait_for_sampling_period(samp_freq)
+        except ValueError:
+            print("smth went wrong wow\n")
+finally:
+    dac.deinit()
