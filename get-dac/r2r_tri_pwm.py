61e081e72c36d98a3a5d40ece8b9594e30310c15
LeonidYutlin <leonidyutlin@phystech.edu>
2026-02-24 21:25:46 +0300

    pwm tri test

---
+++ get-dac/r2r_tri_pwm.py
@@ -0,0 +1,30 @@
+#import pwm_dac as pwm
+import signal_gen as sg
+import time
+
+ampl = 3.0
+freq = 50
+samp_freq = 1000
+
+try:
+    #dac = pwm.PWM_DAC(12, 5000, 3.290, True)
+    state = 0
+    max_state = 100
+    step = 1
+    sign = 1
+    while True:
+        try:
+            print(f"Outputting: {state / 100} \n") 
+            #dac.set_vol(state * ampl)
+            sg.wait_for_sampling_period(samp_freq)
+            state += step * sign
+            if state >= max_state or state <= 0:
+                if sign == 1:
+                    sign = -1
+                else:
+                    sign = 1
+        except ValueError:
+            print("smth went wrong wow\n")
+finally:
+    state = 0
+    #dac.deinit()
