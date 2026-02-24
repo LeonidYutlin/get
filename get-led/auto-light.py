70fc366626557fd67ca6262e7a46733a2dc03a5d
LeonidYutlin <leonidyutlin@phystech.edu>
2026-02-24 21:36:06 +0300

    delete led here

--- get-led/auto-light.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as APDG
-
-APDG.setmode(APDG.BCM)
-bluebert = 26
-straffant = 6
-APDG.setup(straffant, APDG.IN)
-APDG.setup(bluebert, APDG.OUT)
-while True:
-    APDG.output(bluebert, not APDG.input(straffant))
